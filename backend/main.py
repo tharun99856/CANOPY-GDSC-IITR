d589e09cafc8ddb9443a6188451c90cfb8c7b7f6
Tharun Rathod <tharunrathod2005@gmail.com>
2026-06-13 12:18:00 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from dotenv 
-import load_dotenv
+from dotenv import load_dotenv
 import github_api
 import parser
 import ai_layer
