9df396722517530681a2cd9e349468de228c6d82
Tharun Rathod <tharunrathod2005@gmail.com>
2026-06-16 20:22:09 +0530

    Update NodeCard.jsx

--- frontend/src/NodeCard.jsx
+++ frontend/src/NodeCard.jsx
@@ -11,7 +11,7 @@ const TYPE_COLORS = {
 };
 
 const NodeCard = memo(function NodeCard({ data, selected }) {
-  const { label, fullPath, loc, type, searchMatch, searchDim } = data;
+  const { label, loc, type, searchMatch, searchDim } = data;
   const color = TYPE_COLORS[type] || TYPE_COLORS.other;
 
   let cls = "canopy-node";
@@ -20,7 +20,9 @@ const NodeCard = memo(function NodeCard({ data, selected }) {
   if (searchDim) cls += " search-dim";
 
   return (
-          <div className="node-filename" title={label}>
+    <div className={cls} style={{ "--node-type-color": color }}>
+      <Handle type="target" position={Position.Left} style={{ opacity: 0, width: 6, height: 6 }} />
+      <div className="node-filename" title={label}>
         {label}
       </div>
       <span className="loc-badge">{loc} lines</span>
